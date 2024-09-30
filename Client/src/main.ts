24e625239453c6fc57a618f0cbd56ae148111aad
Ziare <zuaresuddith@gmail.com>
2024-09-30 19:37:02 -0400

    removed files and created mock vue project

--- Client/src/main.ts
+++ Client/src/main.ts
@@ -1,10 +1,12 @@
+import 'bulma/css/bulma.css';
+import '@fortawesome/fontawesome-free/css/all.css'
 import './assets/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
-const app = createApp(App)
+const app = createApp(App)//Insantiates app component 
 
 app.use(router)
 
