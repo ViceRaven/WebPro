a24d53a4d2b7c206261cf7731096d2a69abfcb52
Ziare <zuaresuddith@gmail.com>
2024-10-07 14:25:27 -0400

    Simple formatting

--- Client/src/App.vue
+++ Client/src/App.vue
@@ -1,18 +1,23 @@
 <script setup lang="ts">
-import NavBar from './components/NavBar.vue';
-
-
+import NavBar from './components/NavBar.vue'
 </script>
 
 <template>
   <header>
     <NavBar />
-
   </header>
-
-  <RouterView />
+  <div class="container">
+    <RouterView />
+  </div>
 </template>
 
 <style scoped>
-
+body {
+  background-color: aliceblue;
+}
+.container {
+  background-color: white;
+  box-shadow: drop-shadow(0 0 10px rgba(0, 0, 0, 0.1));
+  min-height: 100vh;
+}
 </style>
